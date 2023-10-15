f7c54b95ded9648af71a490ca69950e058ec7ab1
Débora Brandt <->
2023-10-15 22:38:22 +0100

    full OD run from run_OD.sh

--- scripts/run_OD.sh
+++ scripts/run_OD.sh
@@ -1,16 +1,32 @@
-OUTDIR=/home/debora/Dropbox/private/projects/uclpostdoc/bls_sim/slimout/
-
-for s1 in 0.01 0.01 0.2; do
-for s2 in 0.01 0.01 0.2; do
-    for h in 0.5 0.25; do
-        for REP in {1..10000}; do
-            JOB_ID=$REP
-            SEED=$RANDOM
-            slim -d jobid=$JOB_ID -d d_seed=$SEED -d d_repID=$REP -d sel1=$s1 -d sel2=$s2 -d h=$h -d d_folder="'$OUTDIR/OD_N1e3/'" /home/debora/Dropbox/private/projects/uclpostdoc/bls_sim/slim/OD_N1e3.slim
+# grid 1: a finer grid for lower selection parameters
+# 10x10 grid of selection coefficients from 0.01 to 0.1 by 0.01
+# 2x dominance parameters: 0.5 and 0.25
+rep=100
+for s1 in $(seq 0.01 0.01 0.1); do
+    for s2 in $(seq 0.01 0.01 0.1); do
+        for h in 0.5 0.25; do
+            qsub -v s1=$s1,s2=$s2,h=$h,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e3_grid0.01/' -t 1-$rep job_run_OD_N1e3.sh
+            #qsub -v s1=$s1,s2=$s2,h=$h,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e4_grid0.01/' -t 1-$rep job_run_OD_N1e4.sh
+            #qsub -v s1=$s1,s2=$s2,h=$h,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e5_grid0.01/' -t 1-$rep job_run_OD_N1e5.sh
         done
     done
 done
+qsub -v OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e3_grid0.01/' job_plot0.01.sh
+#qsub -v OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e4_grid0.01/' job_plot0.01.sh
+#qsub -v OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e5_grid0.01/' job_plot0.01.sh
+
+# grid 2: a coarser grid for higher selection parameters
+# 10x10 grid of selection coefficients from 0.1 to 1 by 0.1
+# 2x dominance parameters: 0.5 and 0.25
+for s1 in $(seq 0.1 0.1 1); do
+    for s2 in $(seq 0.1 0.1 1); do
+        for h in 0.5 0.25; do
+            qsub -v s1=$s1,s2=$s2,h=$h,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e3_grid0.1/' -t 1-$rep job_run_OD_N1e3.sh
+            #qsub -v s1=$s1,s2=$s2,h=$h,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e4_grid0.1/' -t 1-$rep job_run_OD_N1e4.sh
+            #qsub -v s1=$s1,s2=$s2,h=$h,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e5_grid0.1/' -t 1-$rep job_run_OD_N1e5.sh
+        done
+    done
 done
-
-wait
-
+qsub -v OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e3_grid0.1/' job_plot0.1.sh
+#qsub -v OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e4_grid0.1/' job_plot0.1.sh
+#qsub -v OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e5_grid0.1/' job_plot0.1.sh
