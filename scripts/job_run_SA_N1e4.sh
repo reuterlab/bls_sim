3761b69f977f56be0d5b03e839685235921ef9bb
Débora Brandt <->
2023-10-17 15:34:56 +0100

    update scripts that call slim

--- scripts/job_run_SA_N1e4.sh
+++ scripts/job_run_SA_N1e4.sh
@@ -27,14 +27,15 @@
 mkdir $OUTDIR
 
 SEED=$(shuf -i 1-999999999 -n 1) # slim seed is a random number in that range. Seed number will be written in the slim output file name
-REP=$SGE_TASK_ID # replicate number goes from 1 to the number of array jobs requested with the -t flag above
-slim -d jobid=$JOB_ID -d d_seed=$SEED -d d_repID=$REP -d t1=$s1 -d t2=$s2 -d h=$h -d d_folder="'$OUTDIR/'" /SAN/reuterlab/balsel_detection/bls_sim/slim/AP_N1e3.slim
+REP=$SGE_TASK_ID  # replicate number goes from 1 to the number of array jobs requested with the -t flag above
+slim -d jobid=$JOB_ID -d d_seed=$SEED -d d_repID=$REP -d sf=$s1 -d sm=$s2 -d h=$h -d d_folder="'$OUTDIR/'" -d rand_inv=1 /SAN/reuterlab/balsel_detection/bls_sim/slim/SA_N1e4.slim
 # slim is taking the following values from the command line:
 # jobid=$JOB_ID is the scheduler job ID (will be the same for all tasks in this array)
 # d_seed=$SEED is the slim seed that is a random number in the range 1-999999999
 # d_repID=REP is the replicate number
-# t1 and t2 are the selection coefficients
+# sel1 and sel2 are the selection coefficients
 # h is the dominance parameter
 # d_folder is the subfolder where where the outputs of this particular slim simulations will be written to. Output file names in this folder are:
-# - tree sequences: output_t{t1}-{t2}_h{h}_r{d_repID}_s{d_seed}_j{jobid}.trees
-# - allele frequency files: output_s{t1}-{t2}_h{h}_r{d_repID}_s{d_seed}_j{jobid}.AP_N1e3.txt
+# - tree sequences: output_s{sel1}-{sel2}_h{h}_r{d_repID}_s{d_seed}_j{jobid}_c{checkpoint}.trees
+# - allele frequency files: output_s{sel1}-{sel2}_h{h}_r{d_repID}_s{d_seed}_j{jobid}.SA_N1e4.txt
+# rand_inv is a parameter that says wether we want to randomize the invading allele (A1 or A2). 1 means YES, randomize
--- scripts/run_AP.sh
+++ scripts/run_AP.sh
@@ -5,30 +5,28 @@ rep=100
 for s1 in $(seq 0.01 0.01 0.1); do
     for s2 in $(seq 0.01 0.01 0.1); do
         for h in 0.5 0.25; do
-            qsub -v s1=$s1,s2=$s2,h=$h,OUTDIR='/home/dbrandt/balsel_detection/bls_sim/slimout/AP_N1e3_grid0.01/' -t 1-$rep job_run_AP_N1e3.sh
-            #qsub -v s1=$s1,s2=$s2,h=$h,OUTDIR='/home/dbrandt/balsel_detection/bls_sim/slimout/AP_N1e4_grid0.01/' -t 1-$rep job_run_AP_N1e4.sh
-            #qsub -v s1=$s1,s2=$s2,h=$h,OUTDIR='/home/dbrandt/balsel_detection/bls_sim/slimout/AP_N1e5_grid0.01/' -t 1-$rep job_run_AP_N1e5.sh
+            qsub -v s1=$s1,s2=$s2,h=$h,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N1e3_grid0.01/' -t 1-$rep job_run_AP_N1e3.sh
+            qsub -v s1=$s1,s2=$s2,h=$h,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N1e4_grid0.01/' -t 1-$rep job_run_AP_N1e4.sh
+            qsub -v s1=$s1,s2=$s2,h=$h,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N1e5_grid0.01/' -t 1-$rep job_run_AP_N1e5.sh
         done
     done
 done
-
-# NOTE: on CS cluster, run plotting script with python 3:
-# use an interactive session with qrsh, ex: qrsh -l tmem=4G,h_vmem=4G,h_rt=2:00:00
-# module load python/3.8.5
-python3 plot_grids_0.01.py --outdir /SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N1e3_grid0.01
+qsub -v OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N1e3_grid0.01/' job_plot0.01.sh
+qsub -v OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N1e4_grid0.01/' job_plot0.01.sh
+qsub -v OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N1e5_grid0.01/' job_plot0.01.sh
 
 # grid 2: a coarser grid for higher selection parameters
 # 10x10 grid of selection coefficients from 0.1 to 1 by 0.1
 # 2x dominance parameters: 0.5 and 0.25
-rep=100
 for s1 in $(seq 0.1 0.1 1); do
     for s2 in $(seq 0.1 0.1 1); do
         for h in 0.5 0.25; do
-            qsub -v s1=$s1,s2=$s2,h=$h,OUTDIR='/home/dbrandt/balsel_detection/bls_sim/slimout/AP_N1e3_grid0.1/' -t 1-$rep job_run_AP_N1e3.sh
-            qsub -v s1=$s1,s2=$s2,h=$h,OUTDIR='/home/dbrandt/balsel_detection/bls_sim/slimout/AP_N1e4_grid0.1/' -t 1-$rep job_run_AP_N1e4.sh
-            qsub -v s1=$s1,s2=$s2,h=$h,OUTDIR='/home/dbrandt/balsel_detection/bls_sim/slimout/AP_N1e5_grid0.1/' -t 1-$rep job_run_AP_N1e5.sh
+            qsub -v s1=$s1,s2=$s2,h=$h,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N1e3_grid0.1/' -t 1-$rep job_run_AP_N1e3.sh
+            qsub -v s1=$s1,s2=$s2,h=$h,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N1e4_grid0.1/' -t 1-$rep job_run_AP_N1e4.sh
+            qsub -v s1=$s1,s2=$s2,h=$h,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N1e5_grid0.1/' -t 1-$rep job_run_AP_N1e5.sh
         done
     done
 done
-
-python3 plot_grids_0.1.py --outdir /SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N1e3_grid0.1
+qsub -v OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N1e3_grid0.1/' job_plot0.1.sh
+qsub -v OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N1e4_grid0.1/' job_plot0.1.sh
+qsub -v OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N1e5_grid0.1/' job_plot0.1.sh
