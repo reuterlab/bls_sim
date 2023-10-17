a130fb7189ff796a1d70caf7ef7a002dd233317a
Débora Brandt <->
2023-10-17 14:50:20 +0100

    remove h param from overdominance scripts

--- scripts/run_OD.sh
+++ scripts/run_OD.sh
@@ -4,29 +4,25 @@
 rep=100
 for s1 in $(seq 0.01 0.01 0.1); do
     for s2 in $(seq 0.01 0.01 0.1); do
-        for h in 0.5 0.25; do
-            qsub -v s1=$s1,s2=$s2,h=$h,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e3_grid0.01/' -t 1-$rep job_run_OD_N1e3.sh
-            #qsub -v s1=$s1,s2=$s2,h=$h,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e4_grid0.01/' -t 1-$rep job_run_OD_N1e4.sh
-            #qsub -v s1=$s1,s2=$s2,h=$h,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e5_grid0.01/' -t 1-$rep job_run_OD_N1e5.sh
-        done
+            qsub -v s1=$s1,s2=$s2,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e3_grid0.01/' -t 1-$rep job_run_OD_N1e3.sh
+            qsub -v s1=$s1,s2=$s2,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e4_grid0.01/' -t 1-$rep job_run_OD_N1e4.sh
+            qsub -v s1=$s1,s2=$s2,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e5_grid0.01/' -t 1-$rep job_run_OD_N1e5.sh
     done
 done
 qsub -v OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e3_grid0.01/' job_plot0.01.sh
-#qsub -v OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e4_grid0.01/' job_plot0.01.sh
-#qsub -v OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e5_grid0.01/' job_plot0.01.sh
+qsub -v OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e4_grid0.01/' job_plot0.01.sh
+qsub -v OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e5_grid0.01/' job_plot0.01.sh
 
 # grid 2: a coarser grid for higher selection parameters
 # 10x10 grid of selection coefficients from 0.1 to 1 by 0.1
 # 2x dominance parameters: 0.5 and 0.25
 for s1 in $(seq 0.1 0.1 1); do
     for s2 in $(seq 0.1 0.1 1); do
-        for h in 0.5 0.25; do
-            qsub -v s1=$s1,s2=$s2,h=$h,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e3_grid0.1/' -t 1-$rep job_run_OD_N1e3.sh
-            #qsub -v s1=$s1,s2=$s2,h=$h,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e4_grid0.1/' -t 1-$rep job_run_OD_N1e4.sh
-            #qsub -v s1=$s1,s2=$s2,h=$h,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e5_grid0.1/' -t 1-$rep job_run_OD_N1e5.sh
-        done
+            qsub -v s1=$s1,s2=$s2,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e3_grid0.1/' -t 1-$rep job_run_OD_N1e3.sh
+            qsub -v s1=$s1,s2=$s2,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e4_grid0.1/' -t 1-$rep job_run_OD_N1e4.sh
+            qsub -v s1=$s1,s2=$s2,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e5_grid0.1/' -t 1-$rep job_run_OD_N1e5.sh
     done
 done
 qsub -v OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e3_grid0.1/' job_plot0.1.sh
-#qsub -v OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e4_grid0.1/' job_plot0.1.sh
-#qsub -v OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e5_grid0.1/' job_plot0.1.sh
+qsub -v OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e4_grid0.1/' job_plot0.1.sh
+qsub -v OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/OD_N1e5_grid0.1/' job_plot0.1.sh
