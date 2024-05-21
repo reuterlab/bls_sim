c9bac3320684ba659ce89a85cddf1035116e8f51
Débora Brandt <->
2024-05-21 15:37:37 +0100

    cahnge to variable rec and mut rates

--- scripts/recapitate_selected.py
+++ scripts/recapitate_selected.py
@@ -8,9 +8,9 @@
 # python3 recapitate.py -i [tsfile] -o [output path and prefix] --vcf --nspl 200 --ne $Ne
 # python3 recapitate.py -i /SAN/reuterlab/balsel_detection/bls_sim/slimout_test/OD_N1e3_grid0.01/output_s0.3-0.3_r1_s373688260_j3200352_c4000.trees -o /SAN/reuterlab/balsel_detection/bls_sim/slimout_test/OD_N1e3_grid0.01/output_s0.3-0.3_r1_s373688260_j3200352_c4000_test  --vcf --nspl 200 --ne 1000
 
-import tskit as tsk # installed via mamba 
-import msprime #installed via pip bc mamba didnt work
-import pyslim #installed via pip bc mamba didnt work
+import tskit as tsk # installed via python3 -m pip install tskit 
+import msprime #installed via python3 -m pip install msprime
+import pyslim #installed via python3 -m pip install pyslim
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
@@ -24,11 +24,20 @@ parser.add_argument("--vcf", action="store_true", help="output VCF file")
 parser.add_argument("--nspl", help = "number of samples to output to VCF")
 # added to accommodate variable Ne
 parser.add_argument("--ne", help = "effective population size")
+parser.add_argument("--mu", help = "mutation rate (for neutral mutations)")
+parser.add_argument("--re", help = "recombination rate")
 
 args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
 
 #infile = "../slimout/OD_N1e3_grid0.01/output_s0.1-0.1_r9_s591896120_j3200691_c4000.trees"
 #pref = "../vcf/OD_N1e3_grid0.01/output_s0.1-0.1_r9_s591896120_j3200691_c4000"
+#Ne=20000
+#recrate=1e-6
+#mutrate=1e-7
+
+Ne=int(args.ne)
+recrate=float(args.re)
+mutrate=float(args.mu)
 
 infile = args.input
 
@@ -50,8 +59,7 @@ def tree_heights(ts):
     return sum(heights)/len(heights)
 
 # Recapitate!
-#recap = pyslim.recapitate(ts, ancestral_Ne=int(1000), recombination_rate=0.5e-8)
-recap = pyslim.recapitate(ts, ancestral_Ne=int(args.ne), recombination_rate=0.5e-8) # MAX: updated based on slim scripts
+recap = pyslim.recapitate(ts, ancestral_Ne=Ne, recombination_rate=recrate)
 #recap.dump("../data/"+pref+"_recap.trees")
 recsim = recap.simplify()
 # inserted sanity check:
@@ -60,7 +68,7 @@ recsim = recap.simplify()
 # print(f"Maximum number of roots before recapitation: {orig_max_roots}\n"
 #       f"After recapitation: {recap_max_roots}")
 # mutation rate map setting mutation rate at site under SA (0-based pos=4999) to zero, so that there is no overlap with neutral mutation
-rates_exclmidsite = msprime.RateMap(position=[0, 4999, 5000, 10000], rate=[1e-7, 0, 1e-7]) #increased this becayse Ne*u was too low for Ne=1000 
+rates_exclmidsite = msprime.RateMap(position=[0, 4999, 5000, 10000], rate=[mutrate, 0, mutrate])
 # Ne*u ~ 0.001 in humans, 0.01 in Dmel
 # u=1e-7 -> Ne*u = {1e-4, 1e-3, 1e-2} with Ne = {1e3, 1e4, 1e5}
 mutated = msprime.sim_mutations(recsim, rate=rates_exclmidsite) 
@@ -83,12 +91,13 @@ if args.vcf:
     with open(pref+".vcf" , "w") as vcf:
         mutsim.write_vcf(vcf, individuals=indspl, individual_names = samplenames)
 
-nodespl = list(np.concatenate([mutsim.individual(x).nodes for x in indspl]))
-sfs=mutsim.allele_frequency_spectrum(sample_sets=[nodespl], polarised=True, span_normalise=False)
-np.savetxt(pref+"_SFS.csv", sfs, delimiter=",")
-
-plt.bar(np.arange(mutsim.num_samples + 1), sfs)
-plt.title("Polarised allele frequency spectrum")
-plt.show()
+# some checks/visualizations:
+#nodespl = list(np.concatenate([mutsim.individual(x).nodes for x in indspl]))
+#sfs=mutsim.allele_frequency_spectrum(sample_sets=[nodespl], polarised=True, span_normalise=False)
+#np.savetxt(pref+"_SFS.csv", sfs, delimiter=",")
+#
+#plt.bar(np.arange(mutsim.num_samples + 1), sfs)
+#plt.title("Polarised allele frequency spectrum")
+#plt.show()
 #
 #print(tree_heights(mutsim))
