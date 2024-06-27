cffbc960e4630d3565f5cddde0a6a1d3c20c8773
Débora Brandt <->
2024-06-27 11:50:47 +0100

    fixed AP sims

--- scripts/run_AP_rec.sh
+++ scripts/run_AP_rec.sh
@@ -2,14 +2,14 @@
 # 10x10 grid of selection coefficients from 0.01 to 0.1 by 0.01
 # 2x dominance parameters: 0.5 and 0.25 
 rep=100
-for s1 in $(seq 0.01 0.01 0.1); do
-    for s2 in $(seq 0.01 0.01 0.1); do
+for s1 in 0.01 0.02 0.05 0.09 0.1; do
+    for s2 in 0.01 0.02 0.05 0.09 0.1; do
         for h in 0.5 0.25; do
-            qsub -v s1=$s1,s2=$s2,h=$h,rand_inv=1,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N2k_r1e-6_grid0.01/' -t 1-$rep job_run_AP_N2k_r1e-6.sh
-            qsub -v s1=$s1,s2=$s2,h=$h,rand_inv=1,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N2k_r1e-7_grid0.01/' -t 1-$rep job_run_AP_N2k_r1e-7.sh
+            #qsub -v s1=$s1,s2=$s2,h=$h,rand_inv=1,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N2k_r1e-6_grid0.01/' -t 1-$rep job_run_AP_N2k_r1e-6.sh
+            #qsub -v s1=$s1,s2=$s2,h=$h,rand_inv=1,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N2k_r1e-7_grid0.01/' -t 1-$rep job_run_AP_N2k_r1e-7.sh
             qsub -v s1=$s1,s2=$s2,h=$h,rand_inv=1,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N2k_r1e-8_grid0.01/' -t 1-$rep job_run_AP_N2k_r1e-8.sh
-            qsub -v s1=$s1,s2=$s2,h=$h,rand_inv=1,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N20k_r1e-6_grid0.01/' -t 1-$rep job_run_AP_N20k_r1e-6.sh
-            qsub -v s1=$s1,s2=$s2,h=$h,rand_inv=1,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N20k_r1e-7_grid0.01/' -t 1-$rep job_run_AP_N20k_r1e-7.sh
+            #qsub -v s1=$s1,s2=$s2,h=$h,rand_inv=1,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N20k_r1e-6_grid0.01/' -t 1-$rep job_run_AP_N20k_r1e-6.sh
+            #qsub -v s1=$s1,s2=$s2,h=$h,rand_inv=1,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N20k_r1e-7_grid0.01/' -t 1-$rep job_run_AP_N20k_r1e-7.sh
             qsub -v s1=$s1,s2=$s2,h=$h,rand_inv=1,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N20k_r1e-8_grid0.01/' -t 1-$rep job_run_AP_N20k_r1e-8.sh
         done
     done
@@ -21,11 +21,11 @@ done
 for s1 in $(seq 0.1 0.1 1); do
     for s2 in $(seq 0.1 0.1 1); do
         for h in 0.5 0.25; do
-            qsub -v s1=$s1,s2=$s2,h=$h,rand_inv=1,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N2k_r1e-6_grid0.1/' -t 1-$rep job_run_AP_N2k_r1e-6.sh
-            qsub -v s1=$s1,s2=$s2,h=$h,rand_inv=1,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N2k_r1e-7_grid0.1/' -t 1-$rep job_run_AP_N2k_r1e-7.sh
+            #qsub -v s1=$s1,s2=$s2,h=$h,rand_inv=1,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N2k_r1e-6_grid0.1/' -t 1-$rep job_run_AP_N2k_r1e-6.sh
+            #qsub -v s1=$s1,s2=$s2,h=$h,rand_inv=1,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N2k_r1e-7_grid0.1/' -t 1-$rep job_run_AP_N2k_r1e-7.sh
             qsub -v s1=$s1,s2=$s2,h=$h,rand_inv=1,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N2k_r1e-8_grid0.1/' -t 1-$rep job_run_AP_N2k_r1e-8.sh
-            qsub -v s1=$s1,s2=$s2,h=$h,rand_inv=1,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N20k_r1e-6_grid0.1/' -t 1-$rep job_run_AP_N20k_r1e-6.sh
-            qsub -v s1=$s1,s2=$s2,h=$h,rand_inv=1,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N20k_r1e-7_grid0.1/' -t 1-$rep job_run_AP_N20k_r1e-7.sh
+            #qsub -v s1=$s1,s2=$s2,h=$h,rand_inv=1,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N20k_r1e-6_grid0.1/' -t 1-$rep job_run_AP_N20k_r1e-6.sh
+            #qsub -v s1=$s1,s2=$s2,h=$h,rand_inv=1,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N20k_r1e-7_grid0.1/' -t 1-$rep job_run_AP_N20k_r1e-7.sh
             qsub -v s1=$s1,s2=$s2,h=$h,rand_inv=1,OUTDIR='/SAN/reuterlab/balsel_detection/bls_sim/slimout/AP_N20k_r1e-8_grid0.1/' -t 1-$rep job_run_AP_N20k_r1e-8.sh
         done
     done
