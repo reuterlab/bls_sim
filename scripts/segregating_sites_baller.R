e1d421dd435b490cc3018a56b3b01bbb959b10f7
deboraycb <3833873+deboraycb@users.noreply.github.com>
2024-07-15 10:11:43 +0100

    E[S]

--- scripts/segregating_sites_baller.R
+++ scripts/segregating_sites_baller.R
@@ -1,5 +1,11 @@
-Ne=20000
+Ne=2000
 mu=1e-8
 n=100
-S=14800/10000
+# S = number of segregating sites in concatenated file/number of concatenated files/ sites per file
+S=14800/3000/10000
+
+a = 0 ; for (i in 1:99){a=a+1/i}
+E_S = 4*Ne*mu*a
+S
+E_S
 
