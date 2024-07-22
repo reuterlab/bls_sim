6a6dd5ca1d928968b5bcdb20e25e476d7a4e1b2c
Débora Brandt <->
2024-07-22 16:08:17 +0100

    improve in/output formats

--- scripts/ballerpower.R
+++ scripts/ballerpower.R
@@ -3,18 +3,22 @@ args = commandArgs(trailingOnly=TRUE)
 #selB2file <- '../baller/results/OD_N2k_r1e-8_grid0.1/all_s0.1-0.1_c160000.B2'
 selB2file <- args[1]
 neutralB2file <- args[2]
+seltype <- args[3]
+Ne <- args[4]
+rec <- args[5]
+mut <- args[6]
+s1 <- args[7]
+s2 <- args[8]
+t <- args[9]
 
 neutralB2 <- read.table(neutralB2file, col.names=c("physPos", "genPos", "LR", "xhat", "Ahat", "numSites"))
 selB2 <- read.table(selB2file, col.names=c("physPos", "genPos", "LR", "xhat", "Ahat", "numSites"))
 
-q95 <- quantile(neutralB2$LR, probs=0.95)
-q99 <- quantile(neutralB2$LR, probs=0.99)
-
 alpha <- seq(from=0.01, to=0.25, by=0.01)
 quants <- quantile(neutralB2$LR, probs=1-alpha)
 power <- sapply(quants, function (q) sum(selB2$LR > q)/length(selB2$LR))
+
 png(paste0(selB2file, "_power.png"))
 plot(alpha, power)
 dev.off()
-
-print(power[c("99%", "95%")])
+write.table(cbind(alpha, power), file=paste0(selB2file, "_power.txt"), row.names=F, quote=F)
--- scripts/run_ballerpower.sh
+++ scripts/run_ballerpower.sh
@@ -1,15 +1,21 @@
 # 1. parse baller output for power analysis
-
-## 1.1 neutral sims
 BALDIR=/SAN/reuterlab/balsel_detection/bls_sim/baller/results/
+
+## 1.1 1000 neutral sims fornull
+simpref=neutral_N2k_r1e-8_fornull
+for t in 16000 32000 160000 320000
+do
+awk '$1==4999' $BALDIR/$simpref/output_*_c${t}_*.B2 > $BALDIR/$simpref/all_c${t}.B2
+done
+
+## 1.2 100 neutral sims for testing (taken from the simulations used for genome-wide neutral SFS) 
 simpref=neutral_N2k_r1e-8
 for t in 16000 32000 160000 320000
 do
 awk '$1==4999' $BALDIR/$simpref/output_*_c${t}_*.B2 > $BALDIR/$simpref/all_c${t}.B2
 done
 
-## 1.2 sims with selection need to be parsed per selection grid cell
-BALDIR=/SAN/reuterlab/balsel_detection/bls_sim/baller/results/
+## 1.3 sims with selection need to be parsed per selection grid cell
 simpref=OD_N2k_r1e-8_grid0.1
 
 for s1 in 0.1 0.2 0.5 0.9 1
@@ -20,12 +26,42 @@ done
 done
 done
 
-for s1 in 0.1 0.2 0.5 0.9 1
-do for s2 in 0.1 0.2 0.5 0.9 1
-do
-    for t in 16000 32000 160000 320000
-    do Rscript ballerpower.R $BALDIR/$simpref/all_s${s1}-${s2}_c${t}.B2 $BALDIR/neutral_N2k_r1e-8/all_c${t}.B2
+# 2. power analyses
+#selB2file <- args[1]
+#neutralB2file <- args[2]
+#seltype <- args[3]
+#Ne <- args[4]
+#rec <- args[5]
+#mut <- args[6]
+#s1 <- args[7]
+#s2 <- args[8]
+#t <- args[9]
+simpref=neutral_N2k_r1e-8
+for t in 16000 32000 160000 320000; do
+#Rscript ballerpower.R $BALDIR/$simpref/all_c${t}.B2 $BALDIR/neutral_N2k_r1e-8_fornull/all_c${t}.B2
+qsub -v selB2file=$BALDIR/$simpref/all_c${t}.B2,neuB2file=$BALDIR/neutral_N2k_r1e-8_fornull/all_c${t}.B2 job_ballerpower.sh
+done
+
+for simpref in OD_N2k_r1e-8_grid0.1 AP_N2k_r1e-8_grid0.1 SA_N2k_r1e-8_grid0.1; do
+seltype="OD"
+Ne="2"
+rec="1e-8"
+simpref=${seltype}_N${Ne}k_r${rec}_grid0.1
+for s1 in 0.1 0.2 0.5 0.9 1; do
+for s2 in 0.1 0.2 0.5 0.9 1; do
+    for t in 16000 32000 160000 320000; do
+    Rscript ballerpower.R $BALDIR/$simpref/all_s${s1}-${s2}_c${t}.B2 $BALDIR/neutral_N2k_r1e-8_fornull/all_c${t}.B2
     done
 done
 done
-Rscript ballerpower.R 
+done
+
+for simpref in OD_N2k_r1e-8_grid0.01 AP_N2k_r1e-8_grid0.01 SA_N2k_r1e-8_grid0.01; do
+for s1 in 0.01 0.02 0.05 0.09 0.1; do
+for s2 in 0.01 0.02 0.05 0.09 0.1; do
+    for t in 16000 32000 160000 320000; do
+    Rscript ballerpower.R $BALDIR/$simpref/all_s${s1}-${s2}_c${t}.B2 $BALDIR/neutral_N2k_r1e-8_fornull/all_c${t}.B2
+    done
+done
+done
+done
