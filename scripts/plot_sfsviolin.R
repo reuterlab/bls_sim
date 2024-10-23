d53bae1d6cd7736fc03b5c98446187f148931510
deboraycb <3833873+deboraycb@users.noreply.github.com>
2024-10-23 09:45:42 +0100

    update violin+median line

--- scripts/plot_sfsviolin.R
+++ scripts/plot_sfsviolin.R
@@ -1,8 +1,8 @@
-#Rscript plot_sfsviolin.R ../baller/infiles/OD_N20k_r1e-8_grid0.1/ output_s1-1_r.*c160000_.*.balmixder 100 ../plots/OD_N20k_r1e-8_m1e-8_s1-1_c160000_violinw100.png
-#Rscript plot_sfsviolin.R ../baller/infiles/OD_N20k_r1e-8_grid0.1/ output_s1-1_r.*c160000_.*.balmixder 200 ../plots/OD_N20k_r1e-8_m1e-8_s1-1_c160000_violinw200.png
-#Rscript plot_sfsviolin.R ../baller/infiles/OD_N20k_r1e-8_grid0.1/ output_s1-1_r.*c160000_.*.balmixder 500 ../plots/OD_N20k_r1e-8_m1e-8_s1-1_c160000_violinw500.png
-#Rscript plot_sfsviolin.R ../baller/infiles/OD_N20k_r1e-8_grid0.1/ output_s1-1_r.*c160000_.*.balmixder 1000 ../plots/OD_N20k_r1e-8_m1e-8_s1-1_c160000_violinw1000.png
-#Rscript plot_sfsviolin.R ../baller/infiles/OD_N20k_r1e-8_grid0.1/ output_s1-1_r.*c160000_.*.balmixder 2000 ../plots/OD_N20k_r1e-8_m1e-8_s1-1_c160000_violinw2000.png
+#Rscript plot_sfsviolin.R ../baller/infiles/OD_N20k_r1e-8_grid0.1/ output_s1-1_r.*c160000_.*.balmixder 100 10 ../plots/OD_N20k_r1e-8_m1e-8_s1-1_c160000_violinw100.png
+#Rscript plot_sfsviolin.R ../baller/infiles/OD_N20k_r1e-8_grid0.1/ output_s1-1_r.*c160000_.*.balmixder 200 10 ../plots/OD_N20k_r1e-8_m1e-8_s1-1_c160000_violinw200.png
+#Rscript plot_sfsviolin.R ../baller/infiles/OD_N20k_r1e-8_grid0.1/ output_s1-1_r.*c160000_.*.balmixder 500 10 ../plots/OD_N20k_r1e-8_m1e-8_s1-1_c160000_violinw500.png
+#Rscript plot_sfsviolin.R ../baller/infiles/OD_N20k_r1e-8_grid0.1/ output_s1-1_r.*c160000_.*.balmixder 1000 10 ../plots/OD_N20k_r1e-8_m1e-8_s1-1_c160000_violinw1000.png
+#Rscript plot_sfsviolin.R ../baller/infiles/OD_N20k_r1e-8_grid0.1/ output_s1-1_r.*c160000_.*.balmixder 2000 10 ../plots/OD_N20k_r1e-8_m1e-8_s1-1_c160000_violinw2000.png
 
 library(ggplot2)
 
@@ -10,12 +10,13 @@ args = commandArgs(trailingOnly=TRUE)
 inpath <- args[1]
 inpattern <- args[2]
 winsize <- as.numeric(args[3])
-plotfname <- args[4]
+nbins <- as.numeric(args[4])
+plotfname <- args[5]
 
 #inpath <- "../baller/infiles_testing/OD_N20k_r1e-7_grid0.1_m1e-7/"
 #inpattern <- "output_s1-1_h0_r.*_c160000_spl100.balmixder"
 #inpath <- "../baller/infiles_testing/OD_N20k_r1e-8_grid0.1/"
-#inpattern <- "output_s1-1_r.*_c160000_mut1e-8_spl100.balmixder"
+#inpattern <- "output_s0.1-0.1_r.*_c160000_mut1e-8_spl100.balmixder"
 infiles <- list.files(path=inpath, pattern=inpattern)
 
 dat <- read.table (paste0(inpath, infiles[1]), header=T)
@@ -35,16 +36,16 @@ dat$distance <- abs(dat$physPos-midpos)
 #ggplot(dat)+
 #    geom_point(aes(x=distance, y=maf))
 
-plotviolins <- function (dat, winsize=100, plotfname="violin_windows.png"){
+plotviolins <- function (dat, winsize=100, nbins=10, plotfname="violin_windows.png"){
     s <- 0
     e <- s+winsize
     dat$win[dat$distance==0] <- 0
     winlabels <- c("0")
     i <- 0
-    while (i < 10){
+    while (i < nbins){
         i  <- i+1
         dat$win[ dat$distance > s & dat$distance <= e ] <- i
-        winlabels <- c(winlabels, paste(s,e, sep="-"))
+        winlabels <- c(winlabels, paste(s+1,e, sep="-"))
         s <- s + winsize
         e <- s + winsize
     }
@@ -60,9 +61,10 @@ plotviolins <- function (dat, winsize=100, plotfname="violin_windows.png"){
     ggplot(toplot)+
         geom_jitter(aes(x=win, y=x/n))+
         geom_violin(aes(x=win, y=x/n))+
+        stat_summary(fun.y=median, geom="line", aes(x=win, y=x/n, group=1))+
         xlab("Distance from selected site")+
         ylab("Derived allele frequency")+
         theme(axis.text.x = element_text(angle=20))
     ggsave(plotfname, w=6, h=3)
 }
-plotviolins(dat, winsize=winsize, plotfname=plotfname)
+plotviolins(dat, winsize=winsize, nbins=nbins, plotfname=plotfname)
